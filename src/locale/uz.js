e9996b73c3b38d50bc11be0500f374f03a274640
Inoyatulloh <mwapftp@gmail.com>
2019-06-30 23:05:13 +0500

    Change to latin alphabet from cryllic.
    Nowadays, Latin alphabet is using in Uzbekistan. Because I changed from cryllic.

--- src/locale/uz.js
+++ src/locale/uz.js
@@ -2,12 +2,12 @@ import dayjs from 'dayjs'
 
 const locale = {
   name: 'uz',
-  weekdays: 'Якшанба_Душанба_Сешанба_Чоршанба_Пайшанба_Жума_Шанба'.split('_'),
-  months: 'январ_феврал_март_апрел_май_июн_июл_август_сентябр_октябр_ноябр_декабр'.split('_'),
+  weekdays: 'Yakshanba_Dushanba_Seshanba_Chorshanba_Payshanba_Juma_Shanba'.split('_'),
+  months: 'yanvar_fevral_mart_aprel_may_iyun_iyul_avgust_sentabr_oktabr_noyabr_dekabr'.split('_'),
   weekStart: 1,
-  weekdaysShort: 'Якш_Душ_Сеш_Чор_Пай_Жум_Шан'.split('_'),
-  monthsShort: 'янв_фев_мар_апр_май_июн_июл_авг_сен_окт_ноя_дек'.split('_'),
-  weekdaysMin: 'Як_Ду_Се_Чо_Па_Жу_Ша'.split('_'),
+  weekdaysShort: 'Yak_Dush_Sesh_Chor_Pay_Jum_Shan'.split('_'),
+  monthsShort: 'yan_fev_mar_apr_may_iyun_iyl_avg_sen_okt_noy_dek'.split('_'),
+  weekdaysMin: 'Ya_Du_Se_Ch_Pa_Ju_Sh'.split('_'),
   ordinal: n => n,
   formats: {
     LT: 'HH:mm',
@@ -18,19 +18,19 @@ const locale = {
     LLLL: 'D MMMM YYYY, dddd HH:mm'
   },
   relativeTime: {
-    future: 'Якин %s ичида',
-    past: 'Бир неча %s олдин',
-    s: 'фурсат',
-    m: 'бир дакика',
-    mm: '%d дакика',
-    h: 'бир соат',
-    hh: '%d соат',
-    d: 'бир кун',
-    dd: '%d кун',
-    M: 'бир ой',
-    MM: '%d ой',
-    y: 'бир йил',
-    yy: '%d йил'
+    future: 'Yaqin %s ichida',
+    past: 'Bir necha %s oldin',
+    s: 'fursat',
+    m: 'bir daqiqa',
+    mm: '%d daqiqa',
+    h: 'bir soat',
+    hh: '%d soat',
+    d: 'bir kun',
+    dd: '%d soat',
+    M: 'bir oy',
+    MM: '%d oy',
+    y: 'bir yil',
+    yy: '%d yil'
   }
 }
 
